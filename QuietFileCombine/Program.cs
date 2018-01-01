ec0437b6842c805d9d13fd296e23903f8d0e3319
ArthurWang <jaylooloomi@gmail.com>
2018-01-01 21:01:34 +0800

    Add files via upload

---
+++ QuietFileCombine/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace QuietFileCombine
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var api = QuietFileCombineAPI.GetInstance();
+            string mainFile = @"C:\Video.flv";
+            string subFile1 = @"C:\Action.exe";
+            string subFile2 = @"C:\Action2.exe";
+
+            //Combine distinct file
+            api.CombineDistinctFile(mainFile, subFile1);
+
+            //Extend file which is already combine
+            api.ExtendFile(mainFile, subFile2);
+        }
+
+      
+    }
+}
